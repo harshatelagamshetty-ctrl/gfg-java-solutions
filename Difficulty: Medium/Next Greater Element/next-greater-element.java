6cb149885891f1f512e33d0eac50ac34345883d3
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-08-18 15:10:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.java
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -1,9 +1,9 @@
 class Solution {
     public ArrayList<Integer> nextLargerElement(int[] arr) {
         // code here
-        Stack<Integer> st=new Stack<>();
         ArrayList<Integer> list=new ArrayList<>();
         list.add(-1);
+        Stack<Integer> st=new Stack<>();
         st.push(arr[arr.length-1]);
         for(int i=arr.length-2;i>=0;i--){
             if(arr[i]<st.peek()){
@@ -11,14 +11,15 @@ class Solution {
                 st.push(arr[i]);
             }
             else{
-                while(st.size()!=0 && arr[i]>=st.peek()){
-                    st.pop();
+                while(st.size()!=0 && arr[i]>=st.peek()) st.pop();
+                if(st.size()==0){
+                    list.add(-1);
+                    st.push(arr[i]);
                 }
-                if(st.size()==0) list.add(-1);
                 else{
                     list.add(st.peek());
+                    st.push(arr[i]);
                 }
-                st.push(arr[i]);
             }
         }
         Collections.reverse(list);
